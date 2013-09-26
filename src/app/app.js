90929abfc5e6ae4a950345483b8bdb6b6355e1c1
Reed La Botz <reedlabotz@gmail.com>
2013-09-26 00:31:37 -0400

    Basic template looks ok, nav works.

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,9 @@
 angular.module('app', [
+    'nav',
     'templates.app']);
 
 angular.module('app').config(['$routeProvider', function ($routeProvider) {
+//    $routeProvider.otherwise({redirectTo:'/stuff/today'});
 }]);
 
 angular.module('app').controller('AppCtrl', ['$scope', function($scope) {
---
+++ src/app/nav.js
@@ -0,0 +1,7 @@
+var navModule = angular.module('nav', []);
+navModule.controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    }
+}]);
